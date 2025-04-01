0fd772f7677db174ec9baa3a601156760dcfc07f
ap2024 <126016917+ap2024@users.noreply.github.com>
2025-03-31 19:34:54 -0700

    Automated testing for UI setting for user and admin

--- src/models/test_dashboard.py
+++ src/models/test_dashboard.py
@@ -31,9 +31,33 @@ try:
     settings_link = driver.find_element(By.LINK_TEXT, "Logout")
     settings_link.click()
 
-    time.sleep(3)
+    time.sleep(5)
 
-    print("UI test passed!")
+    print("User UI test passed!")
+
+    # Log in
+    email_input = driver.find_element(By.NAME, "email")
+    password_input = driver.find_element(By.NAME, "password")
+    
+    email_input.send_keys("admin@email.com")
+    password_input.send_keys("password")
+    password_input.send_keys(Keys.RETURN)  # Press Enter to log in
+
+    time.sleep(5)
+
+    #Open dropdown menu
+    menu_button = driver.find_element(By.XPATH, "//div[@class='dropdown']//button")
+    ActionChains(driver).move_to_element(menu_button).click().perform()
+
+    time.sleep(5)
+
+     #Click "Logout" button
+    settings_link = driver.find_element(By.LINK_TEXT, "Logout")
+    settings_link.click()
+
+    time.sleep(5)
+
+    print("Admin UI test passed!")
 
 except Exception as e:
     print(f"Test failed: {e}")
